3fe221566eec0a9fe3503b7854809fb2513aa70e
Caio Silva <vinczken@gmail.com>
2025-08-27 20:48:11 -0300

    main.cpp nao funciona rgb aleatorio

--- computacao_grafica_bccs6/main.cpp
+++ computacao_grafica_bccs6/main.cpp
@@ -2,37 +2,48 @@
 #include <stdio.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
+#include <cstdlib>
+#include <time.h>
 using namespace std;
 
+
 const GLint WIDTH = 800, HEIGHT = 600;
+// VAO é o ponteiro do programa que estou rodando, espaço de memória que representa aquele objeto
+// VBO é o ponteiro para os meus dados brutos, são os dados do objeto a ser representado pelo VAO 
+// shaderProgram é qual programa estou rodando 
+// todo programa pode ser chamado de shader
 GLuint VAO, VBO, shaderProgram;
 
 static const char* vertexShader = "                                 \n\
 #version 330                                                        \n\
                                                                     \n\
 layout(location = 0) in vec2 pos;                                   \n\
+    //^^^^^^^^^^^^^^ é um argumento                                 \n\
                                                                     \n\
 void main() {                                                       \n\
     gl_Position = vec4(pos.x, pos.y, 0.0, 1.0);                     \n\
+  //^^^^^^^^^ -> fica estatico e nao mexe no processo!              \n\
 }                                                                   \n\
 ";
 
 static const char* fragmentShader = "                               \n\
 #version 330                                                        \n\
                                                                     \n\
-uniform in vec4 rgba;                                               \n\
+uniform vec4 tricolor;                                              \n\
+out vec4 color;                                                     \n\
+// fala que ele tem uma saida do tipo e qual o nome                 \n\
                                                                     \n\
 void main() {                                                       \n\
-    color = vec4(rgba);                                             \n\
+    color = vec4(tricolor);                                         \n\
+  //^^^^^^                                                          \n\
 }                                                                   \n\
 ";
 
-void CreateTriangle() {
+void CreateVertices() {
     GLfloat vertices[] = {
-        0.0f, 1.0f,     // Vertice 1
-        -1.0f, -1.0f,   // Vertice 2
-        1.0f, -1.0f     //Vertice 3
+        0.0f, -1.0f,     // Vertice 1
+        1.0f, 1.0f,   // Vertice 2
+        -1.0f, 1.0f     //Vertice 3
     };
 
     glGenVertexArrays(1, &VAO);
@@ -49,9 +60,10 @@ void CreateTriangle() {
     glBindVertexArray(0);
 }
 
-void AddTriangle(GLuint program, const char* charCode, GLenum type) {
+void AddShader(GLuint program, const char* charCode, GLenum type) {
+// Aqui que é Compilar os Shaders!
     GLuint _shader = glCreateShader(type);
-    // _item para sinalizar que eh um item local e temporario, e nao global.
+    // _item para sinalizar que eh um item local e temporario, e não global.
 
     const GLchar* code[1];
     code[0] = charCode;
@@ -60,12 +72,13 @@ void AddTriangle(GLuint program, const char* charCode, GLenum type) {
     glShaderSource(_shader, 1, code, NULL);
     // ( shader, quantos codigos vai rodar, codigo, tamanho (pode ser null))
     glCompileShader(_shader);
-    //  compila o shader
+    // compila o shader
 
 
-    //tratar os erros
+    // tratar os erros
 
     glAttachShader(program, _shader);
+    // adiciona shader no programa
 }
 
 void AddProgram() {
@@ -76,8 +89,8 @@ void AddProgram() {
         return;
     }
 
-    AddTriangle(shaderProgram, vertexShader, GL_VERTEX_SHADER);
-    AddTriangle(shaderProgram, fragmentShader, GL_FRAGMENT_SHADER);
+    AddShader(shaderProgram, vertexShader, GL_VERTEX_SHADER);
+    AddShader(shaderProgram, fragmentShader, GL_FRAGMENT_SHADER);
 
     glLinkProgram(shaderProgram);
 }
@@ -106,14 +119,14 @@ int main() {
         return 1;
     }
 
+    // obtendo os tamanhos reais
     int widthBuffer, heightBuffer;
-
     glfwGetFramebufferSize(window, &widthBuffer, &heightBuffer);
 
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(window); // tornando a janela como principal
 
+    // cria o glew
     glewExperimental = GL_TRUE;
-
     if (glewInit() != GLEW_OK) {
         printf("Erro ao iniciar o GLEW");
 
@@ -122,11 +135,43 @@ int main() {
         return 1;
     }
 
+    // cria a janela tal que vamos escrever o programa
     glViewport(0, 0, widthBuffer, heightBuffer);
+
+
+
+    CreateVertices();
+    AddProgram();
+    // desenhamos a tela enquanto não a fechamos
     while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.4f, 0.1f, 0.2f, 0.9f);
+        // cor de fundo da janela
+        glClearColor(0.55f, 0.15f, 0.48f, 1.0f);
+        //ele que organiza os eventos que vão ser renderizados
         glfwPollEvents();
         glClear(GL_COLOR_BUFFER_BIT);
+
+        srand(time(0));
+        float r = (1.0f + static_cast<float>(rand()) % 10) / 10;
+        float g = 1.0f + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (1.01f - 1.0f)));
+        float b = 1.0f + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (1.01f - 1.0f)));
+        printf("%d, %d, %d\n", r, g, b);
+
+        // ----- desenhando o triangulo
+        //altera a cor do triangulo
+        GLint uniformColor = glGetUniformLocation(shaderProgram, "tricolor");
+            //retorna onde esta o ponteiro para popular, tal a variavel do fragment
+        glUniform4f(uniformColor, r, g, b, 1.0f);
+
+            
+        // inicia o programa
+        glUseProgram(shaderProgram);
+        // chama o VAO, que é onde o triangulo está.
+        glBindVertexArray(VAO);
+            glDrawArrays(GL_TRIANGLES, 0, 3); // Triangulo, começando na posição 0, Numero de pontos 3
+        glBindVertexArray(0);
+
+
+
         glfwSwapBuffers(window);
     }
 
